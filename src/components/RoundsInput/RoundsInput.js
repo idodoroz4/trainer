c9676f25ab44e0917ab849b9d33e6d09b99a92fc
Ido Rosenzwig <irosenzw@redhat.com>
2019-05-14 19:22:08 +0300

    Align roundsInput row
    Signed-off-by: Ido Rosenzwig <irosenzw@redhat.com>

--- src/components/RoundsInput/RoundsInput.js
+++ src/components/RoundsInput/RoundsInput.js
@@ -11,32 +11,6 @@ class RoundsInput extends Component {
             this.props.MinusRound()
         }
     }
-    /*
-     <div className="container" >
-                <br /><br /><br />
-                <div className="row">
-                    <div className="col d-flex justify-content-center">
-                        <h1> Rounds </h1>
-                    </div>
-                </div>
-                <br />
-                <div className="row">
-                    <div className="col d-flex justify-content-md-center">
-                        <button type="button" className="btn btn-outline-secondary" onClick={this.minusRound }>
-                            <i className="minus icon"></i>
-                        </button>
-                        <div style={this.props.rounds > 9 ? { paddingLeft: 120, paddingRight: 120 } : { paddingLeft: 145, paddingRight: 145 }} >
-                            <h1 style={{ fontSize: 80 }}>
-                                {this.props.rounds}
-                            </h1>
-                        </div>
-                        <button type="button" className="btn btn-outline-secondary" onClick={this.props.PlusRound}>
-                            <i className="plus icon"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-    */
     render() {
         return (
             <div className="d-flex flex-row justify-content-around container body formRowWidth">
