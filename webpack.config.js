9c0a136c894faa70bde08e9ba6b28a5b678c1f82
Ido Rosenzwig <irosenzw@redhat.com>
2019-12-18 18:37:47 +0200

    Revert "Split webpack configs to dev, commin and prod"
    This reverts commit d887fc358e1f7c5effd73f0cdaa0a884c7ad9775.

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './build/dist'),
   },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./build"),
+    publicPath: "/public/",
+    watchContentBase: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    open: true,
+    port: 3000
+  },
+  devtool: 'eval-source-map',
   module: {
       rules: [
         {
